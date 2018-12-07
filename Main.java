e56a6da084a3185cef44d72fa174df0687d5f7e1
robertsonandrew2019 <42614650+robertsonandrew2019@users.noreply.github.com>
2018-12-07 14:12:15 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,9 +7,7 @@ public class Main {
         while (n < 10) {
             n++;
             int square = n * n;
-            System.out.printf("%d ", square);
-           }
-        System.out.println();
+            System.out.printf("%d ", square); } System.out.println();
 
         //perfect cubes
         n = 0;
@@ -17,9 +15,7 @@ public class Main {
         while (n < 10) {
             n++;
             int cube = n * n * n;
-            System.out.printf("%d ", cube);
-        }
-        System.out.println();
+            System.out.printf("%d ", cube); } System.out.println();
 
         //fibonacci sequence
         System.out.print("Fibonacci: ");
@@ -32,9 +28,7 @@ public class Main {
             f1 = f2;
             f2 = f3;
             n++;
-            System.out.printf("%d ", f3);
-        }
-        System.out.println();
+            System.out.printf("%d ", f3); } System.out.println();
 
         //prime numbers - needs revision
         System.out.print("Prime numbers: ");
@@ -45,16 +39,12 @@ public class Main {
             while (n <= (num / 2)) {
                 if (num % n == 0) {
                     flag = true;
-                    break;
-                }
-                n++;
-            }
+                    break; }
+                n++; }
             if (!flag) {
-                System.out.printf("%d ", num);
-            }
+                System.out.printf("%d ", num); }
             num++;
-        }
-        System.out.println();
+        } System.out.println();
 
         //triangle numbers
         System.out.print("Triangle Numbers: ");
@@ -62,7 +52,4 @@ public class Main {
         while (n <= 10) {
             int tri = (n * (n + 1) / 2);
             System.out.printf("%d ", tri);
-            n++;
-        }
-    }
-}
+            n++; } } }
